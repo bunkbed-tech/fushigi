bcb8cc0a4cfc9e4e588e5f68ce4a22f729b88064
Tahoe Schrader <tahoeschrader@gmail.com>
2025-09-01 21:19:10 +0900

    fix: add fallback api base for vite

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,16 +1,16 @@
-import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 import adapter from "@sveltejs/adapter-static";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: vitePreprocess(),
-  kit: {
-    adapter: adapter({
-      pages: 'build',
-      assets: 'build',
-      fallback: null
-    })
-  }
+	preprocess: vitePreprocess(),
+	kit: {
+		adapter: adapter({
+			pages: "build",
+			assets: "build",
+			fallback: null,
+		}),
+	},
 };
 
 export default config;
