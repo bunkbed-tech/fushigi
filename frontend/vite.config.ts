1e9db73717076f7ea527b9aaf11d70f8b15742b1
Tahoe Schrader <tahoeschrader@gmail.com>
2025-09-01 21:18:34 +0900

    fix: add fallback api base for vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
 		port: 5173,
 		proxy: {
 			"/api": {
-				target: import.meta.env.VITE_API_BASE,
+				target: process.env.VITE_API_BASE || "http://localhost:8090",
 				changeOrigin: true,
 				secure: false,
 			},
